1c69201cc1ffbff5edb5ef94041dea0ccda62682
Sleaker <sleaker@gmail.com>
2011-06-26 12:01:33 -0700

    Initial version

---
+++ src/net/milkbowl/commanddisabler/CDPlayerListener.java
@@ -0,0 +1,21 @@
+package net.milkbowl.commanddisabler;
+
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerListener;
+
+public class CDPlayerListener extends PlayerListener {
+	CommandDisabler plugin;
+	
+	CDPlayerListener(CommandDisabler plugin) {
+		this.plugin = plugin;
+	}
+	
+	@Override
+	public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+		String cmd = event.getMessage().toLowerCase();
+		for (String disabled : plugin.disabledCmds) {
+			if (disabled.startsWith("/" + cmd)) 
+				event.setCancelled(true);
+		}
+	}
+}
---
+++ src/net/milkbowl/commanddisabler/CommandDisabler.java
@@ -0,0 +1,66 @@
+package net.milkbowl.commanddisabler;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
+
+public class CommandDisabler extends JavaPlugin {
+	
+	protected static final Logger log = Logger.getLogger("Minecraft");
+	public static final String plugName = "[CommandDisabler]";
+	private CDPlayerListener playerListener = new CDPlayerListener(this);
+	protected static Configuration config;
+	protected Set<String> disabledCmds = new HashSet<String>();
+	
+	@Override
+	public void onDisable() {
+		log.info(plugName + " - Disabled successfully!");
+		
+	}
+
+	@Override
+	public void onEnable() {
+		PluginManager pm = this.getServer().getPluginManager();
+		
+		//Try to enable the plugin, disable if there was an error.
+		if (!getConfig()) {
+			pm.disablePlugin(this);
+			log.info(plugName + " - Config could not be created, or was empty.  Please specific commands to disable.");
+		}
+			
+		pm.registerEvent(Event.Type.PLAYER_COMMAND_PREPROCESS, playerListener, Priority.Monitor, this);
+        
+		log.info(plugName + this.getDescription().getName() + " v" + this.getDescription().getVersion() + " enabled successfully.");
+	}
+
+	private boolean getConfig() {
+	       //Check to see if there is a configuration file.
+        File yml = new File(getDataFolder()+"/config.yml");
+
+        if (!yml.exists()) {
+            new File(getDataFolder().toString()).mkdir();
+            try {
+                yml.createNewFile();
+            }
+            catch (IOException ex) {
+                log.info(plugName + " - Cannot create configuration file. And none to load, disabling plugin.");
+                return false;
+            }
+        }	
+        
+        config = getConfiguration();
+        if (!config.getKeys().isEmpty()) {
+        	disabledCmds.addAll(config.getKeys());
+        	return true;
+        } else
+        	return false;
+	}
+}
